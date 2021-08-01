509231fae11a5d4156b04099aec97e2f51a39367
perl <perl@immunant.com>
2021-08-01 11:00:39 -0700

    Tweak python scripts

--- tests/test_utils/parse_dwarfdump.py
+++ tests/test_utils/parse_dwarfdump.py
@@ -1,7 +1,9 @@
+import os
 import subprocess
 
 default_file = "no_return_fn.c.elf"
 tab = "	"
+
 
 def cmd(cmd, stdin=None):
     return subprocess.run(cmd, input=stdin, stdout=subprocess.PIPE) \
@@ -9,9 +11,12 @@ def cmd(cmd, stdin=None):
         .decode("utf-8") \
         .splitlines()
 
-# Get the address of a symbol
+
 def symbol_address(symbol, file=default_file):
-    file = "bin/" + file
+    """
+    Get the address of a symbol
+    """
+    file = os.path.join("bin", file)
     nm_out = cmd(["nm", file])
     for line in nm_out:
         line = line.split()
@@ -19,12 +24,15 @@ def symbol_address(symbol, file=default_file):
             return line[0]
     print("Symbol " + symbol + " not found in " + file)
 
-# Find the offset of the first DWARF entry containing a given pattern
+
 def entry_offset(pattern, file=default_file):
-    file = "strip_bin/" + file
+    """
+    Find the offset of the first DWARF entry containing a given pattern
+    """
+    file = os.path.join("strip_bin", file)
     full_dump = cmd(["llvm-dwarfdump-12", file])
 
-    idx = [i for i,x in enumerate(full_dump) if pattern in x][0]
+    idx = [i for i, x in enumerate(full_dump) if pattern in x][0]
     up_to_pattern = full_dump[0:idx]
 
     all_offsets = [x for x in up_to_pattern if x.startswith('0x')]
@@ -33,33 +41,48 @@ def entry_offset(pattern, file=default_file):
     entry_offset = last_offset.split()[0][0:-1]
     return entry_offset
 
-# Get the DWARF entry at the specified offset
+
 def entry_dump(offset, file=default_file):
-    file = "strip_bin/" + file
+    """
+    Get the DWARF entry at the specified offset
+    """
+    file = os.path.join("strip_bin", file)
     flag = "--debug-info=" + offset
     return cmd(["llvm-dwarfdump-12", flag, file])
 
-# Get the llvm-dwarfdump output for the given functions's entry
+
 def find_entry(function, file=default_file):
+    """
+    Get the llvm-dwarfdump output for the given functions's entry
+    """
     addr = symbol_address(function)
-    offset = entry_offset("DW_AT_low_pc" + tab + "(0x" + addr + ")")
+    offset = entry_offset(f"DW_AT_low_pc{tab}(0x{addr})")
     return entry_dump(offset, file)
 
-# Get all attributes for a function
+
 def attrs(function, file=default_file):
+    """
+    Get all attributes for a function
+    """
     entry = find_entry(function, file)
     return [x.lstrip() for x in entry if "DW_AT_" in x]
 
-# Check if a function has a specified attribute
+
 def has_attr(function, attr, file=default_file):
+    """
+    Check if a function has a specified attribute
+    """
     attr = "DW_AT_" + attr
     for a in attrs(function):
         if a.startswith(attr):
             return True
     return False
 
-# Get the value of a function's attribute
+
 def attr_value(function, attr, file=default_file):
+    """
+    Get the value of a function's attribute
+    """
     attr = "DW_AT_" + attr
     for a in attrs(function):
         if a.startswith(attr):
--- tests/test_utils/test_runner.py
+++ tests/test_utils/test_runner.py
@@ -1,11 +1,13 @@
 from parse_dwarfdump import *
 
+
 def test(test_case):
     print("Test " + test_case + " ... ", end='')
     if eval(test_case):
         print("ok")
     else:
         print("FAILED")
+
 
 tests = open("test_cases", "r")
 for t in tests.read().splitlines():
