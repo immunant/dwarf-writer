2abb724156b71493e376e6706320772e463b64e2
Ayrton Munoz <adm@immunant.com>
2022-03-30 09:36:22 -0400

    Add Q0-Q15 ARM registers for anvill

--- src/anvill/mod.rs
+++ src/anvill/mod.rs
@@ -448,6 +448,23 @@ pub enum ARMRegister {
     S29,
     S30,
     S31,
+
+    Q0,
+    Q1,
+    Q2,
+    Q3,
+    Q4,
+    Q5,
+    Q6,
+    Q7,
+    Q8,
+    Q9,
+    Q10,
+    Q11,
+    Q12,
+    Q13,
+    Q14,
+    Q15,
 }
 
 // TODO: Fill this in. Set variant values to the DWARF register number since
